b2802ac4c2a44ff1c0221f4016b885c855a12c1b
Vaibhav Sharma <hihello44694@gmail.com>
2025-10-13 02:16:11 +0530

    Comment added

--- node/angularexpress/client/src/app.routes.ts
+++ node/angularexpress/client/src/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { AsyncpipeComponent } from './modules/app/components/asyncpipe/asyncpipe.component';
 
 export const routes: Routes = [
-   { path: 'asyncpipe', component: AsyncpipeComponent }, // 
+   { path: 'asyncpipe', component: AsyncpipeComponent }, // for standalone application
 ];
