ff301e38a8636882bac0f9ec1bde54575fa203b6
Vaibhav Sharma <hihello44694@gmail.com>
2025-11-25 23:08:56 +0530

    Updated

--- node/singlefile/arrayOperations.ts
+++ node/singlefile/arrayOperations.ts
@@ -11,3 +11,9 @@ let flat1 = nested.flat(); // One level
 console.log(flat1); // Output: [1, 2, 3, 4, [5]]
 let flat2 = nested.flat(Infinity); // All levels
 console.log(flat2); // Output: [1, 2, 3, 4, 5]
+
+let fruits = ['apple', 'banana', 'cherry', 'coconut'];
+let someFruits = fruits.slice(1, 3);
+fruits.splice(1, 1, 'mango', 'grape'); // Remove 1 item at index 1, add 'mango' and 'grape'
+console.log(someFruits); // Output: ['banana', 'cherry']
+console.log(fruits); // Output: ['apple', 'mango', 'grape', 'cherry']
